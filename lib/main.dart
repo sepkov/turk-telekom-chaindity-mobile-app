126f761a1f18248a7b4f8ca53f7e16bed312df07
Sepkov <furkansonkaya61@gmail.com>
2021-12-20 21:07:16 +0300

    Ana Sayfa tanımlarının eklenmesi

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chaindity Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Chaindity'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +29,420 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget bodyFunction() {
+    switch (_page) {
+      case 0:
+        return MainPage();
+        break;
+      case 1:
+        return AddPage();
+        break;
+      case 2:
+        return Container(color: Colors.orange);
+        break;
+      default:
+        return ProfilePage();
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+              icon: const Icon(Icons.settings))
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: bodyFunction(),
+      bottomNavigationBar: BottomNavigationBar(
+        key: _bottomNavigationKey,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Ana Sayfa'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline), label: 'Ekle'),
+          BottomNavigationBarItem(icon: Icon(Icons.access_time), label: '2FA'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil')
+        ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TTInfoRoute extends StatelessWidget {
+  const TTInfoRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Türk Telekom Kimliğim"),
+      ),
+      body: Column(
+        children: [
+          const Center(
+              child: Card(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "TTNET Kimlik Bilgileri",
+                style: TextStyle(fontSize: 30),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          )),
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Hizmet Num.: 1234567",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Türk Telekom Kimlik Bilgileri",
+                    style: TextStyle(fontSize: 28),
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Tel Num.:+90 555 555 55 55",
+                    style: TextStyle(fontSize: 28),
+                  ),
+                ),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class AddCreditCardRoute extends StatelessWidget {
+  const AddCreditCardRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Second Route"),
         ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                "Kredi Kartı Numarası",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "XXXX-XXXX-XXXX-XXXX",
+                      hintStyle: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ),
+            Center(
+              child: Row(
+                children: const [Text("Wololo")],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Center(
+            child: Card(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Profil Bilgilerim",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+        )),
+        Expanded(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Card(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "Kullanıcı Adı: ShellWizards",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "Pub Key: 0x123456789....",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            )
+          ],
+        ))
+      ],
+    );
+  }
+}
+
+class AddPage extends StatelessWidget {
+  const AddPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Center(
+            child: Card(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Yeni Kimlik Ekle",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+        )),
+        Expanded(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                MainScreenNavigateCard(
+                  label: "Türk Telekom\nKimliği",
+                  imagePath: "assets/main_page/ttlogo.jpeg",
+                ),
+                MainScreenNavigateCard(
+                  label: "Bilişim Vadisi\nKimliği",
+                  imagePath: "assets/main_page/bvlogo.jpeg",
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MainScreenNavigateCard(
+                  label: "Felan Bank\nKimliği",
+                  imagePath: "assets/main_page/ccard.png",
+                  clickFunction: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddCreditCardRoute()),
+                    ),
+                  },
+                ),
+                const MainScreenNavigateCard(
+                  label: "Yeni Kimlik",
+                  imagePath: "assets/main_page/qr.png",
+                ),
+              ],
+            )
+          ],
+        ))
+      ],
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Expanded(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: TextField(
+                  decoration: InputDecoration(suffixIcon: Icon(Icons.search)),
+                ),
+              )),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Icon(Icons.filter_list),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+                  child: Text(
+                    'Türk Telekom',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+                  child: Text(
+                    'Kartlar',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+                  child: Text(
+                    'Sık Kulla.',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MainScreenNavigateCard(
+                    label: "Türk Telekom\nMüşteri Bilgilerim",
+                    imagePath: "assets/main_page/ttlogo.jpeg",
+                    clickFunction: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const TTInfoRoute()),
+                      ),
+                    },
+                  ),
+                  const MainScreenNavigateCard(
+                    label: "Bilişim Vadisi\nGiriş Kartım",
+                    imagePath: "assets/main_page/bvlogo.jpeg",
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  MainScreenNavigateCard(
+                    label: "TR - Açık Kaynak\nOkul Bilgilerim",
+                    imagePath: "assets/main_page/books.png",
+                  ),
+                  MainScreenNavigateCard(
+                    label: "Felan Bank\nBilgilerim",
+                    imagePath: "assets/main_page/ccard.png",
+                  ),
+                ],
+              )
+            ],
+          ))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+@immutable
+class MainScreenNavigateCard extends StatelessWidget {
+  const MainScreenNavigateCard({
+    Key? key,
+    required this.label,
+    required this.imagePath,
+    this.clickFunction,
+  }) : super(key: key);
+
+  final String label;
+  final String imagePath;
+  final void Function()? clickFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: clickFunction,
+        child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8),
+            child: Column(
+              children: [
+                Image.asset(
+                  imagePath,
+                  height: 150,
+                  width: 150,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      label,
+                    ),
+                    const Icon(Icons.navigate_next)
+                  ],
+                )
+              ],
+            )),
+      ),
     );
   }
 }
